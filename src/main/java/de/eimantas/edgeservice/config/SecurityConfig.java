97d05b58940bc42203133b106e99543a44e528d5
eimyss <eimyss@gmail.com>
2018-10-24 18:53:20 +0200

    added trusted ssls

--- src/main/java/de/eimantas/edgeservice/config/SecurityConfig.java
+++ src/main/java/de/eimantas/edgeservice/config/SecurityConfig.java
@@ -60,6 +60,6 @@ class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
         .antMatchers("/expenses*").hasRole("user")
         .antMatchers("/accounts*").hasRole("user")
         .antMatchers("/users*").hasRole("user")
-        .anyRequest().permitAll().and().csrf().disable();
+        .anyRequest().hasRole("user").and().csrf().disable();
   }
 }
